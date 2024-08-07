66de30c97778a00d59ce99a941ffa7c8982b0f19
Ankith Kunduru <1ankithk@gmail.com>
2024-08-07 11:35:55 -0700

    changed publisher

--- gimbal_urdf/scripts/slider_joints.py
+++ gimbal_urdf/scripts/slider_joints.py
@@ -4,7 +4,7 @@ import rospy
 import tf2_ros
 import geometry_msgs.msg
 from std_msgs.msg import Header
-
+import numpy as np
 from geometry_msgs.msg import PoseStamped, Twist
 from geometry_msgs.msg import Pose
 from tf.transformations import quaternion_from_euler
@@ -16,14 +16,13 @@ target_twist = None
 
 def pub_pose(posx, posy, posz, roll, pitch, yaw):
     
-    pub = rospy.Publisher('pose_position', PoseStamped, queue_size=10)
     # sagepose = Pose()
     sagepose = PoseStamped()
-    sagepose.header.frame_id = 'world'
+    sagepose.header.frame_id = 'base_link'
     sagepose.pose.position.x = posx
     sagepose.pose.position.y = posy
     sagepose.pose.position.z = posz
-    quatx, quaty, quatz, quatw = quaternion_from_euler(roll, pitch, yaw)
+    quatx, quaty, quatz, quatw = quaternion_from_euler(np.deg2rad(roll), np.deg2rad(pitch), np.deg2rad(yaw))
     sagepose.pose.orientation.x = quatx
     sagepose.pose.orientation.y = quaty
     sagepose.pose.orientation.z = quatz
@@ -31,8 +30,8 @@ def pub_pose(posx, posy, posz, roll, pitch, yaw):
 
     
     sagepose.header.stamp = rospy.Time.now()
+    return sagepose
     
-    pub.publish(sagepose)
 
 def cb(msg):
     global target_twist
@@ -40,15 +39,17 @@ def cb(msg):
 
 if __name__=='__main__':
     pos_euler_sub = rospy.Subscriber('pos_euler_setpoint',Twist, cb)
+    pub = rospy.Publisher('target_pose', PoseStamped, queue_size=10)
 
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         if target_twist is not None:
-            pub_pose(target_twist.linear.x,
+            sagepose = pub_pose(target_twist.linear.x,
                      target_twist.linear.y,
                      target_twist.linear.z,
                      target_twist.angular.x,
                      target_twist.angular.y,
                      target_twist.angular.z,
                     )
+            pub.publish(sagepose)
         rate.sleep()
